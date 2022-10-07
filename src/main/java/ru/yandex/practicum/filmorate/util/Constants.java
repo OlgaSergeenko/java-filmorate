bc6d0f6585c88278d48447febc8dbd3b1b7b5627
vlolad <vlad19.99@yandex.ru>
2022-10-07 04:19:16 +0300

    add new constants (operationTypes for feed)

--- src/main/java/ru/yandex/practicum/filmorate/util/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Constants.java
@@ -5,4 +5,7 @@ import java.time.LocalDate;
 public class Constants {
 
     public final static LocalDate OLDEST_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    public final static String ADD_OPERATION = "ADD";
+    public final static String REMOVE_OPERATION = "REMOVE";
+    public final static String UPDATE_OPERATION = "UPDATE";
 }

14fae57dc1c38d8a9838f7a4e24712deb65b4cb1
Semyon Senin <ily-senin@yandex.ru>
2022-10-11 17:58:09 +0300

    fix code style

--- src/main/java/ru/yandex/practicum/filmorate/util/validation/SortDirectValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/validation/SortDirectValidator.java
@@ -5,10 +5,12 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.List;
 
 public class SortDirectValidator implements ConstraintValidator<SortDirect, String> {
-    public final void initialize(final SortDirect annotation) {}
+    public final void initialize(final SortDirect annotation) {
+    }
 
     public final boolean isValid(final String sortParam, final ConstraintValidatorContext context) {
         List<String> expectParams = List.of("year", "likes");
+
         return expectParams.contains(sortParam);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/util/validation/SortSearchValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/validation/SortSearchValidator.java
@@ -5,10 +5,12 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.List;
 
 public class SortSearchValidator implements ConstraintValidator<SortSearch, List<String>> {
-public final void initialize(final SortSearch annotation) {}
+    public final void initialize(final SortSearch annotation) {
+    }
 
-public final boolean isValid(final List<String> sortParams, final ConstraintValidatorContext context) {
+    public final boolean isValid(final List<String> sortParams, final ConstraintValidatorContext context) {
         List<String> expectParams = List.of("director", "title");
+
         return expectParams.containsAll(sortParams);
-        }
+    }
 }
